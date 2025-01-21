85334647ef124982f2ca984cce334546aab30246
aasin729 <tkdghks729@gmail.com>
2025-01-22 08:38:21 +0900

    2025.01.22 수정

--- src/components/RelatedSites.js
+++ src/components/RelatedSites.js
@@ -4,58 +4,48 @@ const RelatedSites = () => {
   const sites = [
     {
       name: "국방부",
-      logo: "https://i.namu.wiki/i/IJBNbIG_c2HGNxHCOuS4veoC8lFXO9ZEPsaoznNLWnTGETjIw1nZm9LhiJTxoUa_XT34NBgWySIFU-HC3dUmSnRhCVHBPEIin1_3H4bDbuuInQ3MPTpGhnlMhWUAruxt4_8peMnqlZho11naMN8Oxg.svg",
+      logo: "/img/sublogo1.png", // 로컬 이미지 경로
       link: "https://www.mnd.go.kr",
     },
     {
       name: "육군",
-      logo: "https://i.namu.wiki/i/rdXqteN4mCOP4WKewnoTD9DyDwCDrK6S587A5PJwW31zevHDGkkTA8lpzAEYWzNpHHH4uDWYiOCTfmI0QLC6AmdtGDkmzyFpg4Gi4JE87Pey5SVCmOd8AC39JTAWXNkQyS4EZyyvgOQ0ti_HW_cGeA.svg",
+      logo: "/img/sublogo2.png", // 로컬 이미지 경로
       link: "https://www.army.mil.kr",
     },
     {
       name: "해군",
-      logo: "https://i.namu.wiki/i/8EGKX4e0So6FN6bttSFEgh3p_NUcjrqSiDVIDS7g1D1W0LA-0vpzTh55upGBEe4Es58cgwQk9VV_wyK2h2JEFVmjUAPhH-VKlT5I9RGYaejwbhcrIxGehRAi1ii3b4OQ6KynH5lyQYGA83QWUY_YuQ.svg",
-      link: "https://www.navy.mil.kr",
+      logo: "/img/sublogo3.png", // 로컬 이미지 경로
+      link: "https://rokaf.airforce.mil.kr/sites/airforce/index.do",
     },
     {
       name: "공군",
-      logo: "https://i.namu.wiki/i/uxQOz1V8BQ8EHIctAoD8zFojQL0fVXXOKiLEQLQSioQMQD47X1CyRvMdp6BJ7u0dGcGOpwwhyQqAaNHKuB55wDQcFTyxihxIqpe9DGtaQI_TqDtEOn1OcOZntYXD_Va8_mGtg1kQGcZJxpLI2mmEqQ.svg",
-      link: "https://www.airforce.mil.kr",
+      logo: "/img/sublogo4.png", // 로컬 이미지 경로
+      link: "https://rokaf.airforce.mil.kr/sites/airforce/index.do",
     },
     {
       name: "해병대",
-      logo: "https://i.namu.wiki/i/Oy7slOgI1QOvXTm_2tYZkEG3-5xts4PSNuyq1HDXKMwiRA0OrI7Q5PX_MednUshm14NUPh9edNtK_HOxArUYIZbAPFjI7Tu-9VgDJy5jjBTzcYPPBuK_ET24i8XOvYq98nebUc3izfYWxuMqB1PNIg.svg",
+      logo: "/img/sublogo5.png", // 로컬 이미지 경로
       link: "https://www.rokmc.mil.kr",
     },
     {
       name: "국가보훈처",
-      logo: "https://i.namu.wiki/i/fgyAqUpYDFoAcEYU9ARBPXmD2KfoPY3xlKQtqAqABcXEmEYXgbSlqltu9Wav99YbFV4yRUGrW8PvMHlPYZRMJLDXCyLa16zYZChFRE03xudQv0ZwNVot8HR0vbhpYbf6HbbAv1Xkr8GmtStjArxSVQ.svg",
+      logo: "/img/sublogo6.png", // 로컬 이미지 경로
       link: "https://www.mpva.go.kr",
     },
     {
-      name: "UN평화기념관",
-      logo: "https://i.namu.wiki/i/Cb6UkLb3qHMA74nzZnOm0c7H4aIgKu6GeP6ACaN9mAjGnH9gFRSWe-waz8VEKm9K0CICW2wCdChFcTB8Vv21YfPpDFMPINM3IViN6dNpjZTHzIUlthvFoBgu9yAMfWgpIHdkYTMrDO1J6_RQQGnT2g.svg",
-      link: "https://www.unpm.or.kr",
-    },
-    {
-      name: "전쟁기념관",
-      logo: "https://i.namu.wiki/i/5PXGTAx89OTOr7MoKfiTTR4wfp1FvWhrcnwrjeQU_9tbm8BUwFy8nsdaTGMGrmQEb09cq8Nq-8qUasAo9icixlv9Ac_MjLvX_FbK3lzZ6N8UR3Rf1H5rVh4ywtzavmowbGRAffKyyxFS65GVk9NptQ.svg",
-      link: "https://www.warmemo.or.kr",
-    },
-    {
       name: "국립서울현충원",
-      logo: "https://i.namu.wiki/i/WlULwJ_jDkcpXpGgTQ9-MML3mGPdfqrX5__M5wLQC_t5x-N-0b_rAZFXBJdK4q_69h_Y0lvMeR_zEXvAUm-Vys96lFryRkF5vTrU0zKxo1_Mj5Wb3qWJY0REOWWaBysQOvdm5jcB_qAacDlUlYP51w.svg",
+      logo: "/img/sublogo7.png", // 로컬 이미지 경로
       link: "https://www.snmb.mil.kr",
     },
     {
-      name: "국가공헌협회",
-      logo: "https://i.namu.wiki/i/m_rthQFIqDs5mfv22ibfdaLXu8I3UZZlVzMyqNyBu6_wJbw9vZ5dcdd9lpFObYvVEEn_NPN3YBFGM8BP2v3DbmfD61N5VfF3WHMmAUXULHe2SYlnoz7hHlGIT7vMvC7jDlAZFGCm6PVRHXXJBRMtqQ.svg",
-      link: "https://www.contribution.or.kr",
+      name: "국방부유해발굴단",
+      logo: "/img/sublogo8.png", // 로컬 이미지 경로
+      link: "https://www.withcountry.mil.kr/mbshome/mbs/withcountry/",
     },
     {
-      name: "국방부유해발굴단",
-      logo: "https://i.namu.wiki/i/jtdE05Z7itpFehtTjPeHEd_xJjvQK_HOwDxPMIYpuoAMJgjTnEJ0UD-KXukVTFXld15dx9St6Piy2NvTg04oqHqEnC4aj08goz_UfD2an8bTjjHa5Z1dPAm7fCKvHUKpPlSwwHzrbsMYvbU3T1QGGw.svg",
-      link: "https://www.dpaa.mil",
+      name: "전쟁기념관",
+      logo: "/img/sublogo9.png", // 로컬 이미지 경로
+      link: "https://www.warmemo.or.kr",
     },
   ];
 
@@ -72,7 +62,7 @@ const RelatedSites = () => {
             href={site.link}
             target="_blank"
             rel="noopener noreferrer"
-            className="relative block w-full h-32 bg-gray-200 rounded-md shadow-md hover:shadow-lg transition flex items-center justify-center"
+            className="relative block w-full h-32 rounded-md shadow-md hover:shadow-lg transition flex items-center justify-center"
             aria-label={site.name}
           >
             {site.logo ? (
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -18,12 +18,12 @@ const MainPage = () => {
   }, []);
 
   return (
-    <div className="min-h-screen bg-gray-100">
+    <div className="min-h-screen">
       <Header />
 
       {/* Hero Section (Video Background) */}
       <div
-        className="relative flex items-center justify-center min-h-screen pt-16"
+        className="relative flex items-center justify-center min-h-screen mb-20 pt-16"
         data-aos="fade"
       >
         <video
@@ -40,40 +40,56 @@ const MainPage = () => {
       {/* Section: 한국전쟁 정보 */}
       <section className="py-8 px-4 bg-white" data-aos="fade-up">
         <div className="max-w-screen-xl mx-auto">
-          <h2 className="text-2xl font-semibold text-center mb-8">한국전쟁 (6.25 전쟁)</h2>
-          <p className="text-gray-700 text-center mb-20">
+          <h2 className="text-3xl font-semibold text-center mb-8">한국전쟁 (6.25 전쟁)</h2>
+          <p className="text-gray-500 text-center text-xl font-semibold mb-20">
             1950년 6월 25일 새벽에 북한 공산군이 남북군사분계선이던 38선 전역에 걸쳐 불법 남침함으로써 일어난 한반도에서의 전쟁
           </p>
 
-          {/* Circle Grid Section */}
-          <div className="flex justify-center items-center mb-40">
-            <div className="grid grid-cols-3 gap-0">
-              {Array.from({ length: 6 }).map((_, index) => (
-                <div
-                  key={index}
-                  className="bg-gray-400 text-white font-semibold flex items-center justify-center w-60 h-60 rounded-full shadow-md"
-                  data-aos="fade-up"
-                  data-aos-delay={`${index * 100}`}
-                >
-                  {index + 1}. 남한 공산화를 위한 침략 전쟁
-                </div>
-              ))}
-            </div>
-          </div>
+          
+{/* Circle Grid Section */}
+<div className="flex justify-center items-center mb-40">
+  <div className="grid grid-cols-3 gap-8">
+    {[
+      "남한 공산화를 위한 침략 전쟁",
+      "북한의 치밀한 사전준비에 의한 계획된 전쟁",
+      "민족간의 이념 갈등에 의한 동족상잔의 전쟁",
+      "UN연합군이 본격적으로 개입한 최초의 전쟁",
+      "냉전시대의 개막을 알린 첫 번째 전면전",
+      "미·소 양강대국의 국가이익이 충돌된 국제 대리전",
+    ].map((text, index) => (
+      <div
+        key={index}
+        className="flex items-center justify-center w-60 h-60 rounded-full shadow-lg text-center transition-transform duration-300 transform hover:scale-105 bg-gradient-to-r from-gray-400 to-gray-700"
+        data-aos="fade-up"
+        data-aos-delay={`${index * 100}`}
+      >
+        <span className="px-4 text-white font-semibold text-lg leading-relaxed drop-shadow-lg">
+          {index + 1}. {text}
+        </span>
+      </div>
+    ))}
+  </div>
+</div>
+
+
+
 
           {/* Image and Text Section */}
           <div
             className="flex flex-col md:flex-row items-center gap-6 mb-20"
             data-aos="fade-right"
           >
-            <div className="bg-gray-300 h-full w-full md:w-1/2 rounded-lg flex items-center justify-center text-gray-700 overflow-hidden">
-              <img
-                src={KoreaWar}
-                alt="6.25 전쟁 관련 이미지"
-                className="w-full h-full object-cover"
-              />
+               <div className="bg-gray-300 h-80 w-full md:w-1/2 rounded-lg flex items-center justify-center">
+              <iframe
+                className="w-full h-full rounded-lg"
+                src="https://www.youtube.com/embed/MmsB7vMcjzk?si=ckJw23N8M2VgZfvo"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
             </div>
-            <p className="text-gray-700 text-sm leading-relaxed md:w-1/2">
+            <p className="text-gray-700 text-lg leading-loose md:w-1/2">
               6·25 전쟁은 1950년 6월 25일 북한군의 기습남침으로 시작되어 1953년 7월 27일 휴전이 성립되기까지 약 3년 1개월 2일간 계속되었다.
               기간 중 한반도는 38도선을 각각 3회씩이나 넘나드는 낙동강에서 압록강까지 오르내렸으며, 이로 인한 사상 북측에 145만 명의
               사상자를 포함해 항공기부터 탱크 1,756대의 파괴로 전 국토의 80%가 초토화되었다.
@@ -85,21 +101,18 @@ const MainPage = () => {
             className="flex flex-col md:flex-row items-center gap-6 mb-20"
             data-aos="fade-left"
           >
-            <p className="text-gray-700 text-sm leading-relaxed md:w-1/2">
+            <p className="text-gray-700 text-lg leading-loose md:w-1/2">
               전쟁 결과 한국과 유엔군은 전사 17만 6,000명, 부상 55만 5,000여명, 실종 및 포로 약 4만 2,000명의 인명 피해를 입었고,
               북한과 중국군은 사망 36만 명, 부상 111만여명, 실종 및 포로 14만 5천명의 피해를 입은 것으로 추정된다. 전국의 61만 가옥과
               4203개의 학교와 파괴되고 삶의 터전을 잃은 350만 명의 난민과 300만명의 학동생도들이 타향에서 외로움과 가난, 고통 등을
               유중이 반세기가 지난 지금까지 끝나지 않은 전쟁으로 남아 있다.
             </p>
-            <div className="bg-gray-300 h-48 w-full md:w-1/2 rounded-lg flex items-center justify-center">
-              <iframe
-                className="w-full h-full rounded-lg"
-                src="https://www.youtube.com/embed/영상ID"
-                title="YouTube video player"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-              ></iframe>
+            <div className="bg-gray-300 h-full w-full md:w-1/2 rounded-lg flex items-center justify-center text-gray-700 overflow-hidden">
+              <img
+                src={KoreaWar}
+                alt="6.25 전쟁 관련 이미지"
+                className="w-full h-full object-cover"
+              />
             </div>
           </div>
         </div>
@@ -107,42 +120,86 @@ const MainPage = () => {
 
       {/* Section: Poem */}
       <section
-        className="text-center"
-        style={{
-          backgroundImage: `url("/img/war.jpg")`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-          color: "white",
-          minHeight: "500px",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-        data-aos="fade"
-      >
-        <p className="leading-relaxed shadow-lg font-semibold text-xl bg-opacity-50 p-4 rounded-lg inline-block">
-          이 강산은 내가 지키노라 당신의 그 충정 <br />
-          하늘 보며 힘껏 흔들었던 평화의 깃발 <br />
-          아아 다시 선 이 땅엔, 당신 심은 푸른 소나무 <br />
-          이 목숨 바쳐 큰 나라 위해 끝까지 싸우리라 <br />
-          - 푸른 소나무 -
-        </p>
-      </section>
+         className="text-center relative overflow-hidden"
+         style={{
+           backgroundImage: `url("/img/war.png")`,
+           backgroundSize: "cover",
+           backgroundPosition: "center",
+           backgroundRepeat: "no-repeat",
+           backgroundAttachment: "fixed",
+           color: "white",
+           minHeight: "500px",
+           display: "flex",
+           alignItems: "center",
+           justifyContent: "center",
+         }}
+         data-aos="fade"
+        >       
+         {/* 어두운 오버레이 */}
+         <div
+           style={{
+             position: "absolute",
+             top: 0,
+             left: 0,
+             width: "100%",
+             height: "100%",
+             backgroundColor: "rgba(0, 0, 0, 0.5)",
+             zIndex: 0,
+           }}
+         ></div>
+
+         {/* 텍스트 */}
+         <p
+           className="leading-relaxed shadow-lg font-semibold text-2xl p-4 rounded-lg inline-block"
+           style={{
+             fontFamily: "'Dancing Script', cursive", // Google Font 적용
+             position: "absolute",
+             top: "50%",
+             left: "50%",
+             transform: "translate(-50%, -50%)",
+             zIndex: 1,
+             letterSpacing: "1px", // 약간의 간격 추가
+             textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)", // 텍스트 그림자
+           }}
+         >
+           이 강산은 내가 지키노라 당신의 그 충정 <br />
+           하늘 보며 힘껏 흔들었던 평화의 깃발 <br />
+           아아 다시 선 이 땅엔, 당신 닮은 푸른 소나무 <br />
+           이 목숨 바쳐 큰 나라 위해 끝까지 싸우리라 <br />
+           - 푸른 소나무 -
+         </p>
+         
+         {/* Google Fonts 링크 추가 */}
+         <style>
+           {`
+             @import url('https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;700&display=swap');
+           `}
+         </style>
+       </section>
 
       {/* Section: Ads */}
       <section className="py-8 px-4 bg-white" data-aos="fade-up">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          <div className="p-4 bg-red-100 rounded-lg shadow-md text-center">
-            <p className="text-gray-800 mb-4">
+
+         <div
+            className="p-4 bg-red-100 rounded-lg shadow-md h-60 text-center"
+            style={{
+              backgroundImage: "url('/img/banner1.jpg')",
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          >
+            <p className="text-gray-800 mb-4 mt-20 text-left">
               지금, 참전용사와 그 가족의 안녕을 위해
               <br />
               국가보훈처와 함께해주세요
             </p>
-            <button className="bg-red-500 text-white px-4 py-2 rounded-md">
+            <button className="bg-red-500 text-white  px-4 py-2 rounded-md">
               후원하기
             </button>
           </div>
+
+
           <div className="p-4 bg-green-100 rounded-lg shadow-md text-center">
             <p className="text-gray-800 mb-4">
               영웅의 귀환을
